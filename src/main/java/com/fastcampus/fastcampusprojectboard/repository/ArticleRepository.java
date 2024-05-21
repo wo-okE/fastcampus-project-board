78bd2bba35ac1c41518c83716d089fffd172158f
wo-okE <seugn97@gmail.com>
2024-05-21 15:07:10 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/repository/ArticleRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampus.fastcampusprojectboard.repository;
 
 import com.fastcampus.fastcampusprojectboard.domain.Article;
+import com.fastcampus.fastcampusprojectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 기본적으로 Entity 안에 있는 모든 필드에 대한 검색 기능을 추가해줌
+        QuerydslBinderCustomizer<QArticle>  {// query의 LIKE 와 같이 기능을 추가할 수 있음.
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        //likeIgnoreCase : like '${v}' -- containsIgnoreCase : like '%${v}%' ↓
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // SimpleExpression::eq == (path,value) -> path.eq(value)
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
