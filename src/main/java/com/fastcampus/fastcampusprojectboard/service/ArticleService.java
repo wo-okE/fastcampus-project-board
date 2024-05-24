acd2c7c51994b9a7b36cf7d9ad39a7b41ff5d482
wo-okE <seugn97@gmail.com>
2024-05-24 13:30:02 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/service/ArticleService.java
@@ -0,0 +1,34 @@
+package com.fastcampus.fastcampusprojectboard.service;
+
+import com.fastcampus.fastcampusprojectboard.domain.type.SearchType;
+import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
+import com.fastcampus.fastcampusprojectboard.dto.ArticleUpdateDto;
+import com.fastcampus.fastcampusprojectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {}
+
+    public void updateArticle(long l, ArticleUpdateDto dto) {}
+
+    public void deleteArticle(long articleId) {}
+}
